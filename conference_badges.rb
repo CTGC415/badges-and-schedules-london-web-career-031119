27194a42c683f440edcd54344e91e80e982ea0f6
Cully Curwen <c.t.g.curwen@gmail.com>
2019-03-03 19:56:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,13 +1,10 @@
 # Write your code here.
-#def batch_badge_creator(names)
-#  badges = names.map { |name| "Hello, my name is #{name}." }
-#end
+def batch_badge_creator(names)
+  badges = names.map { |name| "Hello, my name is #{name}." }
+end
 
 def badge_maker(name)
   "Hello, my name is #{name}."
 
 end
-
-def batch_badge_creator(names)
-
-end
+ 
