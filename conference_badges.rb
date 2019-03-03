d164af8ea1a6c4d423db4d9598992eba6e32afc6
Cully Curwen <c.t.g.curwen@gmail.com>
2019-03-03 20:06:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,3 +14,10 @@ def assign_rooms(names)
   names.each { |name| room_list << "Hello, #{name}! You'll be assigned to room #{rooms.shift}!" }
   room_list
 end
+
+def printer(names)
+  badges = batch_badge_creator(names)
+  room_list = assign_rooms(names)
+  badges.each { |badge| puts badge }
+  room_list.each { |room| puts room }
+end
